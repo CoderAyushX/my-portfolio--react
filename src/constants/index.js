754eda1f25969c05b432986415c2dd8cec6d0e76
Ayush kumar <ayushris49271@gmail.com>
2022-08-18 16:08:41 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star , bantu , santu} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Self-taught",
+    content:
+      "I am just a another self taught programmer who really want to code for learning.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Skills",
+    content:
+      "I have a great knowledge of many programming languages and their frameworks",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Teenager",
+    content:
+      "This shit make me feel more confident cause, I am a teenager who love to code.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Thanks for spending time on making a robust design and writing code and tests with best practices.",
+    name: "Abhishek",
+    title: "Brother",
+    img: bantu,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "1010100 1101000 1100001 1101110 1101011 100000 1111001 1101111 1110101 100000 100001 100001 100000.",
+    name: "Abhinek",
+    title: "Brother",
+    img: santu,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Can you help me to write the same awesome code as you did?",
+    name: "Amit & Ajeet",
+    title: "Brother",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Active Supporter",
+    value: "15+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "0.01+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$50",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
